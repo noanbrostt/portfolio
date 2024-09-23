17304047942f3d2357f4ad3a1b46bdf7c927a3d2
Noan Brostt <calielnoan@gmail.com>
2024-09-23 17:35:37 -0300

    profile pic

--- src/App.js
+++ src/App.js
@@ -31,9 +31,9 @@ export default function App() {
                 <section id="about">
                     <About />
                 </section>
-                <section id="timeline">
+                {/* <section id="timeline">
                     <Timeline />
-                </section>
+                </section> */}
                 <section id="skills">
                     <Techstacks />
                 </section>
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./About.css";
 // import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { ThemeContext } from "../../Context/theme";
-import profilePic from "../../assets/Photo_fw14_056.png";
+import profilePic from "../../assets/foto_perfil.png";
 
 export const About = () => {
     const [{ themename }] = React.useContext(ThemeContext);
@@ -29,26 +29,11 @@ export const About = () => {
                             <span className="different">front end</span>
                             , que descobri minha vocação. Hoje sou{" "}
                             <span className="different">Desenvolvedor PHP Full Stack</span>
-                            , sai da engenharia e{" "}
+                            , eu{" "}
                             <span className="different">me formei em ADS</span>{" "}
                             e logo vou finalizar minha{" "}
                             <span className="different">pós-graduação em Front End</span>
                             .
-                            {/* Na faculdade de engenharia foi onde descobri minha{" "}
-                            <span className="different">facilidade com lógica de programação</span>
-                            .
-                            No meu primeiro emprego eu fui o único desenvolvedor de um sistema, e desenvolvi uma{" "}
-                            <span className="different">boa noção de design</span>
-                            , depois que{" "}
-                            <span className="different">me formei em ADS</span>{" "}
-                            eu avancei na carreira e comecei a falar com clientes todo dia, aperfeiçoando minha{" "}
-                            <span className="different">capacidade de traduzir os desejos do cliente em código</span>
-                            . Atualmente,{" "}
-                            <span className="different">estou cursando pós-graduação em front-end</span>{" "}
-                            para expandir ainda mais minhas habilidades.
-                            <br/>
-
-                            Quando não estou programando eu gosto de jogar vôlei, um esporte que pratico há 8 anos. Estou sempre buscando novos desafios, tanto no esporte quanto na minha carreira, com o objetivo de crescer e contribuir com cada projeto que participo. */}
                         </h4>
                     </div>
                 </div>
