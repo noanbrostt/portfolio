6af5a9e4427737f9e07d2a129ae60b31ed92ba2b
Noan Brostt <calielnoan@gmail.com>
2024-09-24 17:51:47 -0300

    "Sobre mim" finalizado

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -19,21 +19,30 @@ export const About = () => {
                     </div>
                     <div className="introduction_datacontainer">
                         <h4>
+                            Olá, seja bem vindo! 🖐🏻
+                            <br />
+                            <br />
                             Meu nome é{" "}
-                            <span className="different">Noan Caliel Brostt</span>{" "}
-                            e moro em{" "}
-                            <span className="different">Curitiba, Paraná (Brasil)</span>
-                            . Conheci a programação na faculdade de engenharia, e após conseguir meu primeiro emprego como programador precisei desempenhar em diversas áreas diferentes, mas foi no{" "}
+                            <span className="different">Noan Caliel Brostt</span>
+                            , moro em{" "}
+                            <span className="different">Curitiba</span>
+                            , sou formado em{" "}
+                            <span className="different">ADS</span>{" "}
+                            e estou finalizando minha{" "}
+                            <span className="different">pós-graduação em Front-End</span>.
+                            <br />
+                            <br />
+                            Conheci a programação na faculdade de engenharia, e após conseguir meu primeiro emprego na área precisei desempenhar em diversas áreas, mas foi no{" "}
                             <span className="different">desenvolvimento web</span>
                             , principalmente no{" "}
-                            <span className="different">front end</span>
-                            , que descobri minha vocação. Hoje sou{" "}
-                            <span className="different">Desenvolvedor PHP Full Stack</span>
-                            , eu{" "}
-                            <span className="different">me formei em ADS</span>{" "}
-                            e logo vou finalizar minha{" "}
-                            <span className="different">pós-graduação em Front End</span>
-                            .
+                            <span className="different">front-end</span>
+                            , que descobri minha vocação.
+                            <br />
+                            <br />
+                            Hoje sou{" "}
+                            <span className="different">Desenvolvedor PHP Full Stack há 3 anos</span>{" "}
+                            e trabalho de forma terceirizada para a{" "}
+                            <span className="different">CAIXA Ecônomica Federal</span>.
                         </h4>
                     </div>
                 </div>
