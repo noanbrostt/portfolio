5718e6bc5a45391bb0e2fe295c60e86e8bbd8521
Noan Brostt <calielnoan@gmail.com>
2026-07-21 22:53:47 -0300

    Updates

--- assets/Skills3D-oAbQlWLN.js
+++ assets/Skills3D-oAbQlWLN.js
@@ -1,4 +1,4 @@
-import{r as Me,g as wx,j as kt,c as Ex,T as fg}from"./index-CXT1ml2Q.js";/**
+import{r as Me,g as wx,j as kt,c as Ex,T as fg}from"./index-xlb1y9V7.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
